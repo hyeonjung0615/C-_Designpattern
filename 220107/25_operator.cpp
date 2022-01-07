348c6ba94c32e76425deba51f8bfc68814d1f89f
hjn <hjn010615@gmail.com>
2022-01-07 17:11:25 +0900

    .

--- 220107/25_operator.cpp
+++ 220107/25_operator.cpp
@@ -16,17 +16,33 @@ public:
     {
     }
 
+    // 2. friend
+    // > frined 선언된 함수나 클래스는 private 접근이 가능합니다.
+    friend Point operator+(const Point& lhs, const Point& rhs);
+
+    // 1. 값을 읽는 함수를 제공합니다.
+    int GetX() const { return x; }
+    int GetY() const { return y; }
+
     // Point Add(const Point& rhs) const
+    #if 0
     Point operator+(const Point& rhs) const
     {
         return Point(x + rhs.x, y + rhs.y);
     }
+    #endif
 
     void Print() const
     {
         cout << x << ", " << y << endl;
     }
 };
+
+Point operator+(const Point& lhs, const Point& rhs)
+{
+    //return Point(lhs.x + rhs.x, lhs.y + rhs.y);
+    return Point(lhs.GetX() + rhs.GetX(), lhs.GetY() + rhs.GetY());
+}
 
 // 연산자 오버로딩
 //  : 연산자를 객체에 대해서 사용할 때,
