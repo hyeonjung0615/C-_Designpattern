92ff4f01fdb2fb7d5f36c51a1c7edcd04920c57e
hjn <hjn010615@gmail.com>
2022-01-14 13:39:53 +0900

    .

--- 220114/46_State.cpp
+++ 220114/46_State.cpp
@@ -1,66 +1,20 @@
+// 46_State.cpp
 #include <iostream>
 using namespace std;
 
 // 아이템 상태에 따른 동작의 변화
-// 방법 1. 아이템의 종류에 따른 조건 분기
-// 2
-
 class Character {
     int gold;
     int item;
+
 public:
-    void SetItem(int i) { item = i; }
-
-    void Run()
-    {
-        if (item == 1)
-            cout << "Run" << endl;
-        else if (item == 2)
-            cout << "Fast Run" << endl;
-        else if (item == 3)
-            cout << "Slow Run " << endl;
-    }
-
-    void Attack() {cout << "Attacl" << endl;}
+    void Run() { cout << "Run" << endl; }
+    void Attack() { cout << "Attack" << endl; }
 };
 
 int main()
 {
-    Chararctor c;
-    c.Run();Z
-    c.Attack();
-}
-
-
-#include <iostream>
-using namespace std;
-
-// 아이템 상태에 따른 동작의 변화
-// 방법 1. 아이템의 종류에 따른 조건 분기
-
-
-class Character {
-    int gold;
-    int item;
-public:
-    void SetItem(int i) { item = i; }
-
-    void Run()
-    {
-        if (item == 1)
-            cout << "Run" << endl;
-        else if (item == 2)
-            cout << "Fast Run" << endl;
-        else if (item == 3)
-            cout << "Slow Run " << endl;
-    }
-
-    void Attack() {cout << "Attacl" << endl;}
-};
-
-int main()
-{
-    Chararctor c;
+    Character c;
     c.Run();
     c.Attack();
 }
